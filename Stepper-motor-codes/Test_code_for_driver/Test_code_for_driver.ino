570ea1daf50a257d2825c773b0e3905d3db46342
Ahmed El Fakharany <afakharany93@gmail.com>
2016-02-14 00:52:26 +0200

    create folder to hold stepper motor codes, create simple .ino code for testing the driver

---
+++ Stepper-motor-codes/Test_code_for_driver/Test_code_for_driver.ino
@@ -0,0 +1,88 @@
+#define DELAY_T 800
+#define BLACK 9
+#define BLUE 11
+#define RED 10
+#define GREEN 8
+
+
+// global variables
+int first =  GREEN;
+int second =  RED; 
+int third =  BLACK ;
+int forth = BLUE;
+
+
+void setup() {
+
+pinMode(first, OUTPUT);
+
+pinMode(second, OUTPUT);
+
+pinMode(third, OUTPUT);
+
+pinMode(forth, OUTPUT);
+
+pinMode(13,OUTPUT);
+
+digitalWrite(first, LOW);
+
+digitalWrite(third, LOW);
+
+digitalWrite(second, LOW);
+
+digitalWrite(forth, LOW);
+digitalWrite(13, LOW);
+}
+
+void loop() {
+digitalWrite(first, HIGH);
+
+digitalWrite(second, LOW);
+
+digitalWrite(third, LOW);
+
+digitalWrite(forth, LOW);
+
+digitalWrite(13,HIGH);
+
+delayMicroseconds(DELAY_T);
+
+
+digitalWrite(first, LOW);
+
+digitalWrite(second, HIGH);
+
+digitalWrite(third, LOW);
+
+digitalWrite(forth, LOW);
+
+digitalWrite(13,LOW);
+
+delayMicroseconds(DELAY_T);
+
+
+digitalWrite(first, LOW);
+
+digitalWrite(second, LOW);
+
+digitalWrite(third, HIGH);
+
+digitalWrite(forth, LOW);
+
+digitalWrite(13,HIGH);
+
+delayMicroseconds(DELAY_T);
+
+
+digitalWrite(first, LOW);
+
+digitalWrite(second, LOW);
+
+digitalWrite(third, LOW);
+
+digitalWrite(forth, HIGH);
+
+digitalWrite(13,LOW);
+delayMicroseconds(DELAY_T);
+
+}
