0169e7fef69aa894bffc1fe9758283e428f9b60b
Ahmed El Fakharany <afakharany93@gmail.com>
2016-03-08 00:55:42 +0200

    test double device with stepper library and comm protocol

--- RaspberryPi/src/StepperMove.py
+++ RaspberryPi/src/StepperMove.py
@@ -4,31 +4,55 @@ from UUGear import *
 UUGearDevice.setShowLogs(0)
 
 device = UUGearDevice('UUGear-Arduino-1239-9170')
+device1 = UUGearDevice('UUGear-Arduino-4713-9982')
 
 if device.isValid():
-	response1 = device.stepper_time_bet_steps(400)
-	print response1
-	sleep(3)
-	response = device.stepper_move(1000)
-	print response
-	
-	sleep(3)
-	response = device.stepper_go_home()
-	print response
+	if device1.isValid():
+		response1 = device.stepper_time_bet_steps(400)
+		print response1
 
-	sleep(10)
-	response = device.stepper_stop()
-	print response
+		response1 = device1.stepper_time_bet_steps(400)
+		print response1+1
+		
+		sleep(3)
+		response = device.stepper_move(1000)
+		print response
 
-	sleep(3)
-	response = device.stepper_resume()
-	print response
+		response = device1.stepper_move(1000)
+		print response+1
+		
+		sleep(3)
+		response = device.stepper_go_home()
+		print response
 
-	sleep(10)
-	response = device.stepper_stop()
-	print response
+		response = device1.stepper_go_home()
+		print response+1
 
-	device.detach()
-	device.stopDaemon()
+		sleep(10)
+		response = device.stepper_stop()
+		print response
+
+		response = device1.stepper_stop()
+		print response+1
+
+		sleep(3)
+		response = device.stepper_resume()
+		print response
+
+		response = device1.stepper_resume()
+		print response+1
+
+		sleep(10)
+		response = device.stepper_stop()
+		print response
+
+		response = device1.stepper_stop()
+		print response+1
+
+		device1.detach()
+		device.detach()
+		device.stopDaemon()
+	else :
+		print "device1 not init"
 else:
 	print 'UUGear device is not correctly initialized.'
