52d51e7c52f761057715dd5ecc6376ea493b8384
Ahmed El Fakharany <afakharany93@gmail.com>
2016-04-06 22:23:43 +0200

    create thermoistor arduino library

---
+++ thermistor-codes/thermistor-lib/thermistor-lib/thermistor-lib.ino
@@ -0,0 +1,22 @@
+#include "Thermistor_3D.h"
+
+#define SERIAL_DEBUG	1
+
+Thermistor_3d therm(A0);
+
+void setup() {
+#if SERIAL_DEBUG
+  Serial.begin(115200);
+#endif
+}
+
+void loop() {
+	float temp = therm.temperature_measurment();
+	#if SERIAL_DEBUG
+	Serial.print("Thermistor temp :"); 
+	Serial.println(temp);
+	#endif
+  	delay(100);
+
+}
+
