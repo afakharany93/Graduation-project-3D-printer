208c9ccbc5087ccec73cfad5366e1f0ff3873e1e
Ahmed El Fakharany <afakharany93@gmail.com>
2016-04-13 02:46:11 +0200

    use pin change interrupt feature

--- endstop_codes/endstop_interrupt/endstop_interrupt.ino
+++ endstop_codes/endstop_interrupt/endstop_interrupt.ino
@@ -1,34 +1,27 @@
-int pin = 2;	//dio pin which accepts external interrupts are either 2 or 3
+#include <avr/interrupt.h>
+
 
 volatile int count= 0;	//to count the umber of interrupts
 void setup() {
-    pinMode(pin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(pin), report, FALLING);
-      Serial.begin(115200);  
+      Serial.begin(115200); 
+     PCICR |= 0b00000010;    // turn on port c change pin interrupt 
+     PCMSK1 |= 0b00000001;    // turn on pin PC0, which is PCINT8, physical pin A0
+
 }
 
 void loop() {
 //Serial.println(1-digitalRead(pin));
 }
 
-void report() {
-//uint8_t oldSREG = SREG;
-//cli();
-//detachInterrupt(digitalPinToInterrupt(pin));
-Serial.println(1-digitalRead(pin));
-//delay(1);
-//Serial.println(1-digitalRead(pin));
 
-//if(!digitalRead(pin))
+ISR(PCINT1_vect)
 {
 	count ++;
 	//if((count%2))
 	{
-		//Serial.print("press ");
-		//Serial.println(millis()); 
-		//Serial.println(count); 
+		Serial.print("press ");
+		Serial.println(millis()); 
+		Serial.println(count); 
 		//Serial.println(1-digitalRead(pin));
 	}
-}
- //SREG = oldSREG;
 }
