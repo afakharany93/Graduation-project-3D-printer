1dfc2d90b1046a31dbe3ff88ae5172909c27ea06
Ahmed El Fakharany <afakharany93@gmail.com>
2016-05-21 18:19:44 +0200

    make extruder heater library

--- extruder-codes/ext-heat-example/ext-heat-example.ino
+++ extruder-codes/ext-heat-example/ext-heat-example.ino
@@ -1,19 +1,13 @@
-#include "Thermistor_3D.h"
-#include "fuzzy.h"
+#include "ext_heat_3D.h"
 #include "ext_Stepper_3D.h"
 
 
-Thermistor_3d temp(A0);
-fuzzy magic(9, 300, 0, 255, 0);
+ext_heat eh;
 ext_stepper_3d extruder;
 
 
-int st_point = 100;
-int input = 0;
-int op = 0;
+int st_point = 260;
 
-float t;
-int st;
 
 /* the ISR function is the one that does the moving of the stepper motor, it outputs the step at the time required */
 ISR(TIMER5_COMPA_vect)          // timer compare interrupt service routine
@@ -25,6 +19,7 @@ ISR(TIMER5_COMPA_vect)          // timer compare interrupt service routine
 void setup() 
 {
 	extruder.permission = 1;
+	eh.ext_heat_permission();
 	Serial.begin(115200);
   	Serial.flush();
  	Serial.println("time\ttemp\te\tep\tce\tcop\top\tsetP\tres\tst");
@@ -32,53 +27,12 @@ void setup()
 
 void loop() 
 {
-	t = temp.temperature_measurment();
-	input = (int) t;
-	if(input != -273)	//when sensor breaks down, -273 is the reading, so this is a protection line
-	{
-		op = op + magic.fuzzy_controller(input, st_point);
-		if(op > 255)
-		{
-			op = 255;
-		}
-		else if (op < 0)
-		{
-		 	op = 0;
-		}
-	}
-	else
-	{
-		op = 0;
-	}
-	analogWrite(3, op);
+	eh.ext_heat_control(st_point);
 
-	if(input >= st_point)
+	if(eh.input >= st_point)
 	{
-		extruder.stepper_flow(extruder.anticlockwise);
+		extruder.stepper_flow(extruder.clockwise);
 	}
 
 
-	st = magic.sample_t_det ();  
-
-	Serial.print(millis());
-	Serial.print("\t");
-	Serial.print(input);
-	Serial.print("\t");
-	Serial.print(magic.error);
-	Serial.print("\t");
-	Serial.print(magic.error_p);
-	Serial.print("\t");
-	Serial.print(magic.ch_error);
-	Serial.print("\t");
-	Serial.print(magic.ch_op);
-	Serial.print("\t");
-	Serial.print(op);
-	Serial.print("\t");
-	Serial.print(st_point);
-	Serial.print("\t");
-	Serial.print(temp.res_debug_val);
-	Serial.print("\t");
-	Serial.println(st);
-
-	delay(st);
 }
