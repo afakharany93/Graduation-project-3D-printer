7f7a9df8b7ea8467b995f0b7336f564c73edc0c3
Ahmed El Fakharany <afakharany93@gmail.com>
2016-06-26 05:50:30 +0200

    stepper move for extruder

--- RaspberryPi/src/StepperMove.py
+++ RaspberryPi/src/StepperMove.py
@@ -7,18 +7,18 @@ device = UUGearDevice('UUGear-Arduino-5658-7598')
 
 if device.isValid():
 	try:
-		response1 = device.stepper_time_bet_steps(1000/95)
+		response1 = device.ext_stepper_time_bet_steps(1000/95)
 		print response1
-		status = device.stepper_status()
+		status = device.ext_stepper_status()
 		print status
 
-		response1 = device.stepper_move(5*800)
+		response1 = device.ext_stepper_move(5*800)
 		print response1
-		status = device.stepper_status()
+		status = device.ext_stepper_status()
 		print status
 
 		for i in range(100) :
-			status = device.stepper_status()
+			status = device.ext_stepper_status()
 			print status
 			sleep(0.1)
 
